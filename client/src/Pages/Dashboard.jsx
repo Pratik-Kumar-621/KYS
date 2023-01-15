706c13d12f0f9c5f367ee7e8cf7dbb828d8a5c78
Nikhil Kumar <nbaghel12@gmail.com>
2023-01-16 00:13:24 +0530

    added summary

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { MenuItem, Select } from "@mui/material";
 import React from "react";
 import Footer from "../Components/Footer";
 import Nav from "../Components/Nav";
+import Summary from "../Components/Summary";
 
 const Dashboard = () => {
   const [personName, setPersonName] = React.useState();
@@ -12,7 +13,7 @@ const Dashboard = () => {
     <div className="dashboard full_screen">
       <Nav />
       <div>
-        <Select defaultValue={"hh"} onChange={handleChange}>
+        {/* <Select defaultValue={"hh"} onChange={handleChange}>
           <MenuItem disabled value="hh">
             Select the Stock
           </MenuItem>
@@ -23,7 +24,20 @@ const Dashboard = () => {
           <MenuItem value="EICHERMOT">EICHERMOT</MenuItem>
           <MenuItem value="RELIANCE">RELIANCE</MenuItem>
           <MenuItem value="TATASTEEL">TATASTEEL</MenuItem>
-        </Select>
+        </Select> */}
+        <Summary
+          name="NIFTY 50"
+          date="13 Jan, 2023"
+          price="17972.15"
+          open="17867.50"
+          previousClose="17858.20"
+          dayClose="17969.20"
+          dayLow="17774.25"
+          dayHigh="17976.40"
+          yearLow="15183.40"
+          yearHigh="18887.60"
+          yearClose="17789.54"
+        />
       </div>
       <Footer />
     </div>
--- server/index.js
+++ server/index.js
@@ -1,70 +1,78 @@
-require('dotenv').config()
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const bodyParser = require("body-parser");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.p3eoj.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const DATBASE_NAME = 'stock-dekho';
-const port = 3000;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const DATBASE_NAME = "stock-dekho";
+const port = 3001;
 const app = express();
 app.use(bodyParser.json());
-client.connect().then(async()=>{
-    console.log('Connected to Database cluster');
-    
-    // await client.close();
-})
+client.connect().then(async () => {
+  console.log("Connected to Database cluster");
+
+  // await client.close();
+});
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/bseCount', async (req, res) => {
-    let x = await client.db(DATBASE_NAME).collection('BSE').countDocuments();
-    res.send(`BSE collection has ${x} documents`);
-})
+app.get("/bseCount", async (req, res) => {
+  let x = await client.db(DATBASE_NAME).collection("BSE").countDocuments();
+  res.send(`BSE collection has ${x} documents`);
+});
 
-
-app.post('/getBSE',async(req,res)=>{
-    
-    let startDate = new Date(req.body.data.startDate)
-    let endDate = new Date(req.body.data.endDate)
-    const findResult = client.db(DATBASE_NAME).collection('BSE').find({
-        Date: {
-          $gte: startDate,
-          $lt: endDate,
-        },
+app.post("/getBSE", async (req, res) => {
+  let startDate = new Date(req.body.data.startDate);
+  let endDate = new Date(req.body.data.endDate);
+  const findResult = client
+    .db(DATBASE_NAME)
+    .collection("BSE")
+    .find({
+      Date: {
+        $gte: startDate,
+        $lt: endDate,
+      },
     });
-    let arr = await findResult.toArray();
-    res.send(arr);
-
-})
-app.post('/getNSE',async (req,res)=>{
-    let startDate = new Date(req.body.data.startDate)
-    let endDate = new Date(req.body.data.endDate)
-    const findResult = client.db(DATBASE_NAME).collection('NSE').find({
-        Date: {
-          $gte: startDate,
-          $lt: endDate,
-        },
+  let arr = await findResult.toArray();
+  res.send(arr);
+});
+app.post("/getNSE", async (req, res) => {
+  let startDate = new Date(req.body.data.startDate);
+  let endDate = new Date(req.body.data.endDate);
+  const findResult = client
+    .db(DATBASE_NAME)
+    .collection("NSE")
+    .find({
+      Date: {
+        $gte: startDate,
+        $lt: endDate,
+      },
     });
-    let arr = await findResult.toArray();
-    res.send(arr);
-})
-app.post('/getCompany',async(req,res)=>{
-    let startDate = new Date(req.body.data.startDate)
-    let endDate = new Date(req.body.data.endDate)
-    let companyName = req.body.data?.companyName;
-    const findResult = client.db(DATBASE_NAME).collection(companyName).find({
-        Date: {
-          $gte: startDate,
-          $lt: endDate,
-        },
+  let arr = await findResult.toArray();
+  res.send(arr);
+});
+app.post("/getCompany", async (req, res) => {
+  let startDate = new Date(req.body.data.startDate);
+  let endDate = new Date(req.body.data.endDate);
+  let companyName = req.body.data?.companyName;
+  const findResult = client
+    .db(DATBASE_NAME)
+    .collection(companyName)
+    .find({
+      Date: {
+        $gte: startDate,
+        $lt: endDate,
+      },
     });
-    let arr = await findResult.toArray();
-    res.send(arr);
-})
+  let arr = await findResult.toArray();
+  res.send(arr);
+});
 
-
-
-app.listen(port,()=>{
-    console.log(`Server is up and running at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is up and running at port ${port}`);
+});
 
 // TODOS
-// implement AUTH 
+// implement AUTH
