426832fc3e9763374d99731cb6331c546447ec7b
Pratik-Kumar-621 <119cs0030@iiitk.ac.in>
2023-01-16 12:50:35 +0530

    Dashboard Final Integration

---
+++ client/src/Components/Summary.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
+
+const Summary = ({
+  name,
+  date,
+  price,
+  open,
+  previousClose,
+  dayClose,
+  dayLow,
+  dayHigh,
+  yearLow,
+  yearHigh,
+  yearClose,
+}) => {
+  const changePrice = parseFloat(price) - parseFloat(previousClose);
+
+  return (
+    <div className="summary">
+      <h2>{name}</h2>
+      <hr />
+      <div className="details">
+        <div className="price">
+          <h1>{price}</h1>
+          <h3 className={`${changePrice > 0 ? "green" : "red"} growth`}>
+            <ArrowDropUpIcon />
+            {changePrice.toFixed(2)} ({(changePrice / 100).toFixed(2)}%)
+          </h3>
+          <h5>As on {date}</h5>
+        </div>
+
+        <div className="range">
+          <div className="byDay">
+            <h3>Day Range</h3>
+            <div className="data">
+              <h4>{dayLow}</h4>
+              <h4>{dayHigh}</h4>
+            </div>
+
+            <div className="rangeValue">
+              <h4 className="red">L</h4>
+              <input type="range" min={dayLow} max={dayHigh} value={dayClose} />
+              <h4 className="green">H</h4>
+            </div>
+          </div>
+
+          <div className="byYear">
+            <h3>52 week Range</h3>
+
+            <div className="data">
+              <h4>{yearLow}</h4>
+              <h4>{yearHigh}</h4>
+            </div>
+
+            <div className="rangeValue">
+              <h4 className="red">L</h4>
+              <input
+                type="range"
+                min={yearLow}
+                max={yearHigh}
+                value={yearClose}
+              />
+              <h4 className="green">H</h4>
+            </div>
+          </div>
+
+          {/* <div className="returns">
+            <h3>Returns</h3>
+            <h4>
+              YTD <span>3.55%</span>
+            </h4>
+          </div> */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Summary;
--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,31 +1,188 @@
-import { MenuItem, Select } from "@mui/material";
-
-import React from "react";
+import { Button, MenuItem, Select } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
 import Footer from "../Components/Footer";
 import Nav from "../Components/Nav";
 import Graph from "../Components/Graph";
+import { useRef } from "react";
+import moment from "moment";
+import Summary from "../Components/Summary";
 
 const Dashboard = () => {
-  const [personName, setPersonName] = React.useState();
+  // States
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const [personName, setPersonName] = useState("getNSE");
+  const [startDate, setStartDate] = useState(new Date("2018-01-15"));
+  const [endDate, setEndDate] = useState(new Date("2023-01-13"));
+  const [data, setData] = useState(null);
 
-  const handleChange = (e) => {};
+  const stockName = `${personName === "getNSE" ? "Nifty 50" : ""}${
+    personName === "getBSE" ? "Bombay Stock Exchange" : ""
+  }${personName === "ASHOKLEY" ? "Ashok Leyland Ltd" : ""}${
+    personName === "CIPLA" ? "Cipla Ltd" : ""
+  }${personName === "EICHERMOT" ? "Eicher Motors Ltd" : ""}${
+    personName === "RELIANCE" ? "Reliance Industries Ltd" : ""
+  }${personName === "TATASTEEL" ? "Tata Steel Limited" : ""}`;
+
+  // Apis Cache
+  const cache = useRef({});
+
+  // State Changings
+  const handleChange = (e) => {
+    setPersonName(e.target.value);
+  };
+  const HandleOneMonth = () => {
+    const newEnd = moment(endDate).subtract(1, "M").format("YYYY-MM-DD");
+    setStartDate(new Date(newEnd));
+  };
+  const HandleSixMonth = () => {
+    const newEnd = moment(endDate).subtract(6, "M").format("YYYY-MM-DD");
+    setStartDate(new Date(newEnd));
+  };
+  const HandleOneYear = () => {
+    const newEnd = moment(endDate).subtract(12, "M").format("YYYY-MM-DD");
+    setStartDate(new Date(newEnd));
+  };
+  const Reset = () => {
+    setStartDate(new Date("2018-01-15"));
+  };
+  const Handle18Month = () => {
+    const newEnd = moment(endDate).subtract(18, "M").format("YYYY-MM-DD");
+    setStartDate(new Date(newEnd));
+  };
+
+  // Apis Content
+  const baseUrl = `http://localhost:3001/${
+    personName === "getNSE" || personName === "getBSE"
+      ? personName
+      : "getCompany"
+  }`;
+  const compName = `${
+    personName === "getNSE" || personName === "getBSE" ? "" : personName
+  }`;
+
+  // Api call
+  React.useEffect(() => {
+    if (
+      cache.current[`${personName}_${startDate.getTime()}_${endDate.getTime()}`]
+    ) {
+      setData(
+        cache.current[
+          `${personName}_${startDate.getTime()}_${endDate.getTime()}`
+        ]
+      );
+    } else {
+      setData();
+      axios
+        .post(baseUrl, {
+          data: {
+            startDate: startDate,
+            endDate: endDate,
+            companyName: compName,
+          },
+        })
+        .then((response) => {
+          cache.current = {
+            ...(cache.current ?? {}),
+            [`${personName}_${startDate.getTime()}_${endDate.getTime()}`]:
+              response.data,
+          };
+          setData(response.data);
+        });
+    }
+  }, [personName, startDate, endDate]);
+  console.log(data);
+  console.log(data && data[data?.length]);
+
   return (
     <div className="dashboard full_screen">
       <Nav />
-      <div>
-        <Select defaultValue={"hh"} onChange={handleChange}>
-          <MenuItem disabled value="hh">
-            Select the Stock
+      <div className="dashboard_body">
+        <Select
+          value={personName}
+          onChange={handleChange}
+          className="select_str"
+        >
+          <MenuItem className="menu_str" value="getNSE">
+            <div>NSE</div>
           </MenuItem>
-          <MenuItem value="NSE">NSE</MenuItem>
-          <MenuItem value="BSE">BSE</MenuItem>
-          <MenuItem value="ASHOKLEY">ASHOKLEY</MenuItem>
-          <MenuItem value="CIPLA">CIPLA</MenuItem>
-          <MenuItem value="EICHERMOT">EICHERMOT</MenuItem>
-          <MenuItem value="RELIANCE">RELIANCE</MenuItem>
-          <MenuItem value="TATASTEEL">TATASTEEL</MenuItem>
+          <MenuItem className="menu_str" value="getBSE">
+            <div>BSE</div>
+          </MenuItem>
+          <MenuItem className="menu_str" value="ASHOKLEY">
+            <div>ASHOKLEY</div>
+          </MenuItem>
+          <MenuItem className="menu_str" value="CIPLA">
+            <div>CIPLA</div>
+          </MenuItem>
+          <MenuItem className="menu_str" value="EICHERMOT">
+            <div>EICHERMOT</div>
+          </MenuItem>
+          <MenuItem className="menu_str" value="RELIANCE">
+            <div>RELIANCE</div>
+          </MenuItem>
+          <MenuItem className="menu_str" value="TATASTEEL">
+            <div>TATASTEEL</div>
+          </MenuItem>
         </Select>
-        <Graph />
+        {data && (
+          <Summary
+            name={`${stockName} (${
+              personName === "getNSE" || personName === "getBSE"
+                ? `${personName[3]}${personName[4]}${personName[5]}`
+                : personName
+            })`}
+            date={`${new Date(data[data?.length - 1].Date).getDate()}  ${
+              monthNames[new Date(data[data?.length - 1].Date).getMonth()]
+            } ${new Date(data[data?.length - 1].Date).getFullYear()}`}
+            price={data[data?.length - 1].Close}
+            open={data[data?.length - 1].Open}
+            previousClose={data[data?.length - 2].Close}
+            dayClose={data[data?.length - 1].Close}
+            dayLow={data[data?.length - 1].Low}
+            dayHigh={data[data?.length - 1].High}
+            yearLow="15183.40"
+            yearHigh="18887.60"
+            yearClose="17789.54"
+          />
+        )}
+        {data ? (
+          <>
+            <Graph arr={data} />
+          </>
+        ) : (
+          <>Loading</>
+        )}
+        <div className="filters_date">
+          <Button className="filter1" onClick={HandleOneMonth}>
+            1M
+          </Button>
+          <Button className="filter2" onClick={HandleSixMonth}>
+            6M
+          </Button>
+          <Button className="filter3" onClick={HandleOneYear}>
+            1Y
+          </Button>
+          <Button className="filter4" onClick={Handle18Month}>
+            18M
+          </Button>{" "}
+          <Button className="filter5" onClick={Reset}>
+            Reset
+          </Button>
+        </div>
       </div>
       <Footer />
     </div>
